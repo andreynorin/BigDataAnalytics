27cf4372cb57cb2700ef2feaa635c57e14d8d3d7
andreynorin <andreynorin@gmail.com>
2022-11-09 17:09:17 -0500

    Add files via upload

--- HadoopCodeTest/mapper.py
+++ HadoopCodeTest/mapper.py
@@ -5,11 +5,11 @@ import re
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     line = line.strip()
-    line = re.sub(r'[^ w s]','', line ))   # Remove punctuation
-    line = re.sub(r d+]','', line)         # Remove numbers
+    line = re.sub(r'[^\w\s]','', line) # Remove punctuation
+    line = re.sub(r'[\d+]','', line) # Remove numbers
     # split the line into words
     words = line.split()
     # increase counters
     for word in words:
-    # tab delimited the word count is always 1
-        print '%s t%s' % (word, 1)
+        # tab-delimited the word count is always 1
+        print '%s\t%s' % (word, 1)
