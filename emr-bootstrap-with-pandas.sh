520677eca3e990a1f61fcc48093dd9f6db40759e
Andrey Norin <andreynorin@gmail.com>
2022-12-04 18:57:06 -0500

    adding emr config code

---
+++ emr-bootstrap-with-pandas.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x -e
+
+echo -e 'export PYSPARK_PYTHON=/usr/bin/python3
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+export SPARK_JARS_DIR=/usr/lib/spark/jars
+export SPARK_HOME=/usr/lib/spark' >> $HOME/.bashrc && source $HOME/.bashrc
+
+sudo python3 -m pip install awscli boto spark-nlp sklearn spark-sklearn
+
+sudo yum  install git
+
+git clone git@github.com:andreynorin/BigDataAnalytics.git
+
+set +x
+exit 0
