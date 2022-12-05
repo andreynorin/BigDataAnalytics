ec2cb079da78674f5d2da5c2f6377ae509c27e59
Andrey Norin <andreynorin@gmail.com>
2022-12-04 19:38:25 -0500

    Fixed PySpark code bugs, Predict set now works

--- PySparkPipelineToS3.py
+++ PySparkPipelineToS3.py
@@ -8,21 +8,22 @@ from pyspark.ml import Pipeline
 import pyspark.sql.functions as f
 from pyspark.sql.functions import col
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col,lit
 
-parDF1=spark.read.parquet("hdfs:///GoogleTrainingData/tcilen6wu42p3p6m3q5n22m53u.parquet")
-parDF1.count()
+tempDF1=spark.read.parquet("hdfs:///GoogleTrainingData/tcilen6wu42p3p6m3q5n22m53u.parquet")
+tempDF1.count()
 
-parDF2=spark.read.parquet("hdfs:///CC-News-En-Titles-Only/CC-NEWS-20160827145159-00003_ENG.parquet")
-parDF2.count()
+tempDF2=spark.read.parquet("hdfs:///CC-News-En-Titles-Only/CC-NEWS-20160827145159-00003_ENG.parquet")
+tempDF2.count()
 
-parDF1 = parDF1.withColumn('index', f.monotonically_increasing_id())
+tempDF1 = tempDF1.withColumn('index', f.monotonically_increasing_id())
 
-trainDataset    = parDF1
-predictDataset  = parDF2.withColumnRenamed('Title','title')
+trainDataset    = tempDF1
+predictDataset  = tempDF2.withColumn("topic", lit("some topic")).select(["topic","title"])
 
 # splitting the dataset int 160k and 40k records for train/test
-trainDataset = parDF1.sort('index').limit(160000)
-testDataset = parDF1.sort('index', ascending = False).limit(40000)
+trainDataset = tempDF1.sort('index').limit(160000)
+testDataset = tempDF1.sort('index', ascending = False).limit(40000)
 
 trainDataset.groupBy("topic").count().orderBy(col("count").desc()).show()
 testDataset.groupBy("topic").count().orderBy(col("count").desc()).show()
@@ -52,7 +53,7 @@ preds_df.show(20)
 
 # make predictions against CC-News-En dataset
 preds = bert_clf_pipelineModel.transform(predictDataset)
-preds_df = preds.select('topic','Title','class.result')
+preds_df = preds.select('topic','title','class.result')
 preds_df.count()
 preds_df.show(20)
 
